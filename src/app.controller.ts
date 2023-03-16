23fba1eaf5f6bf46a5e2a8715cb8d18870ba6878
Kathir <kathir1007@gmail.com>
2023-03-16 22:10:54 +0530

    Initial commit with working form filling code

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('formFill')
+  async executePuppeteer(): Promise<string> {
+    // const data = await this.appService.execute();
+    const data = await this.appService.execute();
+    return '';
+  //   return data;
+  }
+
+  @Post('newFormTest')
+  async create(@Body() requestBody: any) {
+    const result = await this.appService.executeData(requestBody);
+    return result;
+  }
+}
